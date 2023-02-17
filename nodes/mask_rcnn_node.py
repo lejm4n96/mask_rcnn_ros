9b6a1af7a02cfd4ef99232ffd02ccc0b0ed98965
Martin Günther <martin.guenther@dfki.de>
2023-02-17 17:22:41 +0100

    Output visualization image at same resolution as input

--- nodes/mask_rcnn_node.py
+++ nodes/mask_rcnn_node.py
@@ -15,6 +15,12 @@ from mask_rcnn_ros.mrcnn import model as modellib
 from mask_rcnn_ros.mrcnn import visualize
 from mask_rcnn_ros.msg import Result
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from skimage.measure import find_contours
+
+
 class InferenceConfig(Config):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
@@ -147,13 +153,27 @@ class MaskRCNNNode(object):
         from matplotlib.backends.backend_agg import FigureCanvasAgg
         from matplotlib.figure import Figure
 
-        fig = Figure()
+        dpi = mpl.rcParams['figure.dpi']
+        default_height = plt.rcParams['figure.figsize'][1]   # default figure height (4.8 inches)
+        height, width = image.shape[:2]
+
+        # Adjust dpi to preserve original image size. This also scales fonts appropriately.
+        # This formula works out because height is in pixels (dots) and default_height is in inches,
+        # giving dpi (dots per inch).
+        dpi = height / default_height
+
+        # What size does the figure need to be in inches to fit the image?
+        figsize = width / float(dpi), height / float(dpi)
+
+        # Create a figure of the right size with one axes that takes up the full figure
+        fig = Figure(figsize=figsize, dpi=dpi)
+        ax = fig.add_axes([0, 0, 1, 1])
+
         canvas = FigureCanvasAgg(fig)
-        axes = fig.gca()
-        visualize.display_instances(image, result['rois'], result['masks'],
-                                    result['class_ids'], self._class_names,
-                                    result['scores'], ax=axes,
-                                    colors=self._class_colors)
+        display_instances(image, result['rois'], result['masks'],
+                          result['class_ids'], self._class_names,
+                          result['scores'], ax=ax,
+                          colors=self._class_colors)
         fig.tight_layout()
         canvas.draw()
         result = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')
@@ -167,6 +187,95 @@ class MaskRCNNNode(object):
         if self._msg_lock.acquire(False):
             self._last_msg = msg
             self._msg_lock.release()
+
+
+def display_instances(image, boxes, masks, class_ids, class_names,
+                      scores=None, title="",
+                      figsize=(16, 16), ax=None,
+                      show_mask=True, show_bbox=True,
+                      colors=None, captions=None):
+
+    """
+    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
+    masks: [height, width, num_instances]
+    class_ids: [num_instances]
+    class_names: list of class names of the dataset
+    scores: (optional) confidence scores for each box
+    title: (optional) Figure title
+    show_mask, show_bbox: To show masks and bounding boxes or not
+    figsize: (optional) the size of the image
+    colors: (optional) An array or colors to use with each object
+    captions: (optional) A list of strings to use as captions for each object
+    """
+    # Number of instances
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+
+    # If no axis is passed, create one and automatically call show()
+    auto_show = False
+    if not ax:
+        _, ax = plt.subplots(1, figsize=figsize)
+        auto_show = True
+
+    # Generate random colors
+    colors = colors or visualize.random_colors(N)
+
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    ax.set_ylim(height, 0)
+    ax.set_xlim(0, width)
+    ax.axis('off')
+    ax.set_title(title)
+
+    masked_image = image.astype(np.uint32).copy()
+    for i in range(N):
+        color = colors[i]
+
+        # Bounding box
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        if show_bbox:
+            p = visualize.patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                            alpha=0.7, linestyle="dashed",
+                                            edgecolor=color, facecolor='none')
+            ax.add_patch(p)
+
+        # Label
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            label = class_names[class_id]
+            caption = "{} {:.3f}".format(label, score) if score else label
+        else:
+            caption = captions[i]
+        ax.text(x1, y1 + 8, caption,
+                color='w', size=11, backgroundcolor="none")
+
+        # Mask
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = visualize.apply_mask(masked_image, mask, color)
+
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = find_contours(padded_mask, 0.5)
+        for verts in contours:
+            # Subtract the padding and flip (y, x) to (x, y)
+            verts = np.fliplr(verts) - 1
+            p = Polygon(verts, facecolor="none", edgecolor=color)
+            ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
+    if auto_show:
+        plt.show()
+
 
 def main():
     rospy.init_node('mask_rcnn')
